e09d6da57f872fd93b449511a1073504881e121b
wrdzy <James_NB@outlook.com>
2025-03-30 20:51:13 +0100

    Update i cant take this anymore.lua

--- i cant take this anymore.lua
+++ i cant take this anymore.lua
@@ -24,7 +24,7 @@ if isstarted then
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
 local InterfaceManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/InterfaceManager.lua"))()
-local Version = "vers. 1.3.5"
+local Version = "vers. 1.4.0"
 
 local Admins = {
     8205778977
